77b7a900c1e53ef20f85d7222c985226e55ec29c
rasmi <rasminoel@gmail.com>
2024-01-24 15:22:23 +0300

    Edit Body.js and css

--- client/pages/Body.js
+++ client/pages/Body.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import { PiWarehouseBold } from "react-icons/pi";
+import { IoCarSportOutline } from "react-icons/io5";
+import Link from 'next/link'
 
 function Body() {
-  return (
-    <div>Body</div>
-  )
+    return (
+        <div className='body'>
+            <h1>Welcome to the no. 1 care dealership website!!</h1>
+            <h2>Here you can find the best car that suits your needs</h2>
+            <h3>Let us start by visiting our delership options or view available cars...</h3>
+            <br />
+            <br />
+            <div className="navlinks" >
+                <Link href='/dealerships' className="dealers">
+                    <PiWarehouseBold size={60} />
+                    <h4>Dealers</h4>
+                </Link>
+
+                <Link href='/vehicles' className="cars">
+                    <IoCarSportOutline size={60} />
+                    <h4>Cars</h4>
+                </Link>
+            </div>
+        </div>
+    )
 }
 
 export default Body
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import "@/styles/Header.css";
+import "@/styles/Body.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
