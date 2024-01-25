24655f5d86209cec125c50d5ab96888113c14ec6
rasmi <rasminoel@gmail.com>
2024-01-25 15:48:08 +0300

    Update landing page

--- client/pages/Body.js
+++ client/pages/Body.js
@@ -1,6 +1,10 @@
-import React from 'react'
-import Image from 'next/image'
-import Background1 from '../Public/Background/Background1.jpg'
+import React from 'react';
+import Image from 'next/image';
+import { PiWarehouseBold } from 'react-icons/pi';
+import { IoCarSportOutline } from 'react-icons/io5';
+import { GoCodeReview } from "react-icons/go";
+import { IoMdLogIn } from "react-icons/io";
+import Background1 from '../Public/Background/Background1.jpg';
 
 
 function Body() {
@@ -15,23 +19,46 @@ function Body() {
                     quality={100}
                 />
             </div>
-            <div className='body__container'>
-                <div className='body__container__text'>
-                    <h1>Find the best car for you</h1>
-                    <p>Find the best car that suits your needs. We have a wide range of cars for you to choose from.</p>
-                </div>
-                <div className='body__container__icons'>
-                    <div className='body__container__icons__icon'>
-                        <PiWarehouseBold />
-                        <p>Wide range of cars</p>
+            <div className='nav__links'>
+                <div className='nav__links__container'>
+                    <div className='nav__links__container__icon'>
+                        <PiWarehouseBold size={30} />
                     </div>
-                    <div className='body__container__icons__icon'>
-                        <IoCarSportOutline />
-                        <p>Best prices</p>
+                    <div className='nav__links__container__text'>
+                        <h3>Dealerships</h3>
+                    </div>
+                </div>
+                <div className='nav__links__container'>
+                    <div className='nav__links__container__icon'>
+                        <IoCarSportOutline size={30} />
+                    </div>
+                    <div className='nav__links__container__text'>
+                        <h3>Cars on Sale</h3>
+                    </div>
+                </div>
+                <div className='nav__links__container'>
+                    <div className='nav__links__container__icon'>
+                        <GoCodeReview size={30} />
+                    </div>
+                    <div className='nav__links__container__text'>
+                        <h3>Car Reviews</h3>
+                    </div>
+                </div><div className='nav__links__container'>
+                    <div className='nav__links__container__icon'>
+                        <IoMdLogIn size={30} />
+                    </div>
+                    <div className='nav__links__container__text'>
+                        <h3>Login | Sign Up</h3>
                     </div>
                 </div>
             </div>
-        </div>
+
+            <div className='body__container'>
+                <div className='body__container__text'>
+                    <h1>Used and New cars for sale in Nairobi...</h1>
+                </div>
+            </div>
+        </div >
     )
 }
 
